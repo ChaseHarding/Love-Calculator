1bc52d60ef460c48838f0a65bb5918c510450814
ChaseHarding <chaseharding965@gmail.com>
2024-06-01 17:17:06 -0700

    storing both inputs in one variable for logic use

--- love_calculator.py
+++ love_calculator.py
@@ -1,3 +1,8 @@
+#Welcome to the Love Calculator
+#Below is the code users interact with on page load
 print("The love calculator is calculating your score...")
 name1 = input("What is your name?: ")
 name2 = input("What is your partners name?: ")
+
+#This is where the fun begins
+combined_names = name1 + name2 
