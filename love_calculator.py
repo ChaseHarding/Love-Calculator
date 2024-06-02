8f8a04562f39de66216cad79a88bddc15ca65084
ChaseHarding <chaseharding965@gmail.com>
2024-06-01 17:21:04 -0700

    checking for number of occurences where the letters in true occur

--- love_calculator.py
+++ love_calculator.py
@@ -7,3 +7,12 @@ name2 = input("What is your partners name?: ")
 #This is where the fun begins
 combined_names = name1 + name2 
 lower_names = combined_names.lower()
+
+#lets check how many times the letters in 'true' appear
+t = lower_names.count("t")
+r = lower_names.count("r")
+u = lower_names.count("u")
+e = lower_names.count("e")
+
+#storing this value in a new variable for the final answer
+first_digit = t + r + u + e
