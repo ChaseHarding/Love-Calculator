c8ea162f1e2c91311041df6029f684d2c51e4259
ChaseHarding <chaseharding965@gmail.com>
2024-06-01 18:23:12 -0700

    Added conditional statements for varying love scores

--- love_calculator.py
+++ love_calculator.py
@@ -28,3 +28,11 @@ second_digit = l + o + v + e
 
 #final variable for love score
 score = int(str(first_digit) + str(second_digit))
+
+#conditional statements for different scores
+if (score < 10) or (score > 90):
+    print(f"Your score is {score}, you go together like coke and mentos.")
+elif (score >= 40) and (score <= 50):
+    print(f"Your score is {score}, you are alright together.")
+else:
+    print(f"Your score is {score}.")
